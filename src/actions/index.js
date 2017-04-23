fd08bf9e9f782f342ca88bb8f7fcf417219b2700
Michael Sadahiro <michaelsadahiro@gmail.com>
2017-04-23 15:26:38 -0700

    add axios and made urls

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,14 @@
 import API_KEY from './API.js'
+import axios from 'axios';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
+export const FETCH_WEATHER = 'FETCH_WEATHER'
 
+export function fetchWeather(city){
+    const url = `${ROOT_URL}&q=${city},us`
+    const request = axios.get(url);
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    }
+}
